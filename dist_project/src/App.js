af5abdbb378d8e95eef09b3758f4d47c55c1b09d
zaiba19 <zaibaiqbal1@gmail.com>
2020-05-18 17:55:32 -0400

    centered page

--- dist_project/src/App.js
+++ dist_project/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import './App.css';
+
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/Navbar";
 import WordsList from './components/wordList';
 import AddWord from './components/wordAdd';
+ 
 
 
 
--- dist_project/src/components/wordAdd.js
+++ dist_project/src/components/wordAdd.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from 'axios'
 
 export class wordAdd extends Component {
+    
     constructor(props) {
         super(props);
 
@@ -24,11 +26,14 @@ export class wordAdd extends Component {
         // })
     }
     onChangeWord(e) {
+        e.preventDefault();
         this.setState({
             word: e.target.value
         });
     }
     onChangetarnslation(e) {
+        e.preventDefault();
+
         this.setState({
             translation: e.target.value
         });
@@ -39,6 +44,7 @@ export class wordAdd extends Component {
             word: this.state.word,
             translation: this.state.translation
         }
+        
         console.log(definitions)
         axios.post('http://localhost:5000/words/add', definitions)
             .then(res => console.log(res.data));
@@ -49,7 +55,9 @@ export class wordAdd extends Component {
 
             <div style={{ position: 'center' }}>
 
-                <h3>Your Word of the Day</h3>
+                <h3 className="title">Your Word of the Day</h3>               
+                    <br></br>
+                    <br></br>                   
                 <form onSubmit={this.onSubmit}>
                     <div className="form-group">
                         <label>Your Word</label>
@@ -60,18 +68,23 @@ export class wordAdd extends Component {
                             onChange={this.onChangeWord}
                         />
 
-                    </div>
+                    </div>                    
+                    <br></br>
+                   
                     <div className="form-group">
 
-                    </div>
-                    <label>translation</label>
+                    </div> 
+                    <div className="form-group">
+
+                    <label className="form-group ">Translation</label>
                     <input
                         type="text"
                         value={this.state.translation}
                         onChange={this.onChangetarnslation}
                     />
+                    </div>
                     <div className="form-group">
-                        <input type="submit" value="Get tranlation" className="btn btn-primary" />
+                        <input type="submit" value="Get translation" className="btn btn-primary" />
                     </div>
                 </form>
 
